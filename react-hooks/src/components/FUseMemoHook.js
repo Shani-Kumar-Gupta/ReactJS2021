d526d4e9f2f2e62560f16a9cdb3947a5482eefeb
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-06-15 00:08:56 +0530

    useMemo() functional hook

--- react-hooks/src/components/FUseMemoHook.js
+++ react-hooks/src/components/FUseMemoHook.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function FUseMemoHook() {
+    /*
+        1. Optimize expensive operation
+        2. Referential equality
+    */
+    const factorial = (num) => {
+
+    }
+   const [counter, setCounter] = useState(1);
+   const result = factorial(counter);
+   const [name, setName] = useState("");
     return (
         <div>
             <h2>useMemo() functional hook</h2>
+            <h1>Factorial of {counter} is: <span>{result}</span></h1>
+            <div>
+                <button onClick={() => setCounter(counter -1)}>decrement</button>
+                <button onClick={() => setCounter(counter + 1)}>increment</button>
+            </div>
+            <hr/>
+            <div>
+                <label>Enter Name</label>
+                <input
+                    type="text"
+                    placeholder="enter name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <p>My name is {name}</p>
+            </div>
         </div>
     )
 }
