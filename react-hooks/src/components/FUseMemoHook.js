7c872bef4a2abc4e7f632d44a9101ccf001d2dbd
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-06-14 23:57:02 +0530

    useMemo() functional hook

---
+++ react-hooks/src/components/FUseMemoHook.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FUseMemoHook() {
+    return (
+        <div>
+            <h2>useMemo() functional hook</h2>
+        </div>
+    )
+}
+
+export default FUseMemoHook;
